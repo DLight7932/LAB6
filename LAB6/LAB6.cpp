e1104cc8dc5170e58ece7d5bd748fc4bdca0cc84
DLight <dlight7932@gmail.com>
2023-01-08 00:47:42 +0200

    commit

---
+++ LAB6/LAB6.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Method_1(int* mas, int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += mas[i];
+    return sum;
+}
+
+int Method_2(int* mas, int size)
+{
+    do
+    {
+        for (int i = 0; i < size / 2; i++)
+        {
+            int end = size - 1 - i;
+            mas[i] += mas[end];
+        }
+        size = size / 2 + size % 2;
+    } while (size > 1);
+    return mas[0];
+}
+
+int main()
+{
+    const int size = 2000;
+    int mas[size];
+    for (int i = 0; i < size; i++)
+        mas[i] = i;
+
+    cout << "Simple sum: " << Method_1(mas, size) << endl;
+
+    cout << "Dubling sum: " << Method_2(mas, size) << endl;
+
+    return 0;
+}
